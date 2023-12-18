b5d351b364c3ec8a91ed39ea40dfba99cb48fde1
Rosa Ponce Espín <yosoyrosaponce@gmail.com>
2023-12-18 11:23:19 +0100

    fist const

---
+++ js-adakitten/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const js-list = document.querySelector('js-list');
+
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="https://dev.adalab.es/gato-siames.webp"
+    alt="gatito"
+  />
+  <h3 class="card_title">Anastacio</h3>
+  <h4 class="card_race">Siamés</h4>
+  <p class="card_description">
+            Porte elegante, su patrón de color tan característico y sus ojos
+            de un azul intenso, pero su historia se remonta a Asía al menos
+            hace 500 años, donde tuvo su origen muy posiblemente.
+   </p>
+</article>
+</li>`;
+
+const kittenTwo = `<li class="card"> 
+            <img
+              class="card_img"
+              src="https://dev.adalab.es/sphynx-gato.webp"
+              alt="sphynx-cat"
+            />
+            <h3 class="card_title">Fiona</h3>
+            <h4 class="card_race">Sphynx</h4>
+            <p class="card_description">
+              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+              hasta con pinta de alienígena han llegado a definir a esta raza
+              gatuna que se caracteriza por la «ausencia» de pelo.
+            </p>
+          </li>`;
