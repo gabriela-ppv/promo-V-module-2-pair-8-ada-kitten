dc301d8fdd54ec64f9eac30d6c9acb203dbdf6be
Rosa Ponce Espín <yosoyrosaponce@gmail.com>
2023-12-19 11:21:39 +0100

    day2

--- js-adakitten/main.js
+++ js-adakitten/main.js
@@ -2,22 +2,43 @@
 
 const jslist = document.querySelector('.js-list');
 
+/* const kittenOne = `<li> 
+  ${kittenOneImage} + ${kittenOneName} + ${kittenOneRace} + ${kittenOneDesc}`
+</li>; */
+
 const kittenOne = `<li class="card">
+
 <article>
-  <img
-    class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
-    alt="gatito"
-  />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">
-            Porte elegante, su patrón de color tan característico y sus ojos
-            de un azul intenso, pero su historia se remonta a Asía al menos
-            hace 500 años, donde tuvo su origen muy posiblemente.
-   </p>
+
+<img
+
+class="card_img"
+
+src="https://dev.adalab.es/gato-siames.webp"
+
+alt="gatito"
+
+/>
+
+<h3 class="card_title">Anastacio</h3>
+
+<h4 class="card_race">Siamés</h4>
+
+<p class="card_description">
+
+Porte elegante, su patrón de color tan característico y sus ojos
+
+de un azul intenso, pero su historia se remonta a Asía al menos
+
+hace 500 años, donde tuvo su origen muy posiblemente.
+
+</p>
+
 </article>
+
 </li>`;
+
+
 
 const kittenTwo = `<li class="card"> 
             <img
@@ -50,3 +71,32 @@ const kittenThree = `<li class="card">
 
   const jsListElement = document.querySelector('.js-list');
   jsListElement.innerHTML = `${kittenOne} + ${kittenTwo} +${kittenThree}`;
+
+
+  const kittenOneImage = 'https://dev.adalab.es/gato-siames.webp';
+  const kittenOneName = 'Anastacio';
+  const kittenOneDesc = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+  const kittenOneRace =  'Siamés';
+
+  const kittenTwoImage = 'https://dev.adalab.es/sphynx-gato.webp';
+  const kittenTwoName = 'Fiona';
+  const kittenTwoDesc = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
+  const kittenTwoRace =  'Sphynx';
+
+  const kittenThreeImage = 'https://dev.adalab.es/maine-coon-cat.webp';
+  const kittenThreeName = 'Cielo';
+  const kittenThreeDesc = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+  const kittenThreeRace =  'Maine Coon';
+
+
+ /*  const dogOne = document.querySelector('.dogBox');
+dogOne.innerHTML = `<ul>
+ <li> ${firstDogName}<li/>
+ <img src=${firstDogImage} />
+
+<li> ${secondDogName}<li/>
+<img src=${secondDogImage} />
+
+ <li > ${thirdDogName}<li/>  
+ <img src=${thirdDogImage} />
+ <ul/>`; */
